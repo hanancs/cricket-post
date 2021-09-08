18b1d50d47fbac009814f7c417f6e1ea85cd0101
hanancs <benhanan.subendran@gmail.com>
2021-09-09 04:55:19 +0530

    checked the git log

--- data/projectsData.js
+++ data/projectsData.js
@@ -15,6 +15,14 @@ const projectsData = [
     imgSrc: '/static/images/time-machine.jpg',
     href: '/blog/the-time-machine',
   },
+  {
+    title: 'A Search Engine',
+    description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.google.com',
+  },
 ]
 
 export default projectsData
